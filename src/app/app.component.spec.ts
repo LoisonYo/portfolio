2480ee9b316a779ebeefb8e678f18a7ac71afbb7
LoisonYo <yohann.loison@outlook.com>
2025-01-26 16:19:33 +0100

    chores: remove test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,10 +21,4 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     expect(component).toBeTruthy();
   });
-
-  it('should have the nav bar component', () => {
-    const element = fixture.nativeElement;
-    const navElement = element.querySelector('nav-bar');
-    expect(navElement).toBeTruthy();
-  });
 });
