b35ca8e274e593a140fa14d1c001bc77805f7ce3
LoisonYo <yohann.loison@outlook.com>
2025-01-26 16:33:00 +0100

    chores: add the profile picture component to the about me component

--- src/app/about-me/about-me.component.spec.ts
+++ src/app/about-me/about-me.component.spec.ts
@@ -21,4 +21,10 @@ describe('AboutMeComponent', () => {
   it('should create the about-me component', () => {
     expect(component).toBeDefined();
   });
+
+  it('should have the profile picture component', () => {
+    const element = fixture.nativeElement;
+    const profilePictureElement = element.querySelector('profile-picture');
+    expect(profilePictureElement).toBeTruthy();
+  });
 });
--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ProfilePictureComponent } from '../about-me/profile-picture/profile-picture.component';
 
 @Component({
   selector: 'about-me',
   standalone: true,
+  imports: [ProfilePictureComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
